4c78745fe83b56ec1c051dde7346adfa750feecd
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2023-06-19 23:36:53 +0300

    v8

---
+++ v8/app.js
@@ -0,0 +1,11 @@
+let outter = null;
+let run = function () {
+  let inner = outter;
+  let unused = function () {
+    if (inner) console.log('hi');
+  };
+  outter = {
+    longStr: new Array(1000000).join('*')
+  };
+};
+setInterval(run, 1000);
