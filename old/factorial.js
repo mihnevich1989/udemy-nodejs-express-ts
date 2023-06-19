7476249bbe552cb0e906a167faa103406408a6d6
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2023-06-20 00:13:56 +0300

    weather-cli

--- old/factorial.js
+++
@@ -1,6 +0,0 @@
-module.exports = function factorial(n) {
-  if (n == 1 || n == 0) {
-    return 1;
-  }
-  return factorial(n - 1) * n;
-};
--- old/forkWorker.js
+++
@@ -1,8 +0,0 @@
-process.on('message', (msg) => {
-  if (msg == "disconnect") {
-    process.disconnect();
-    return;
-  }
-  console.log('Client got message: ', msg);
-  process.send('Pong');
-});
