bf14935d220cb18efaf8ce51b8123879de0e38c1
Dima Mihnevich <dima.mihnevich@gmail.com>
2023-06-21 10:19:12 +0300

    Работа с файловой системой

--- weather-cli/services/storage-service.js
+++ weather-cli/services/storage-service.js
@@ -1,16 +1,35 @@
 import { homedir } from 'os';
-import { join, basename, dirname, extname, relative, isAbsolute, resolve, sep } from 'path';
+import { join } from 'path';
+import { promises } from 'fs';
 
-const filePath = join(homedir(), '/weather-cli/weather-data.json');
-const saveKeyValue = (key, value) => {
-  console.log(basename(filePath)); // weather-data.json
-  console.log(dirname(filePath)); // C:\Users\mikhn\weather-cli
-  console.log(extname(filePath)); // .json
-  console.log(relative(filePath, dirname(filePath))); // ..
-  console.log(isAbsolute(filePath)); // true
-  console.log(resolve('..')); // D:\IT\Udemy\NodeJS-Express\NodeJS Express JS TS
-  console.log(sep); // \
+const filePath = join(homedir(), 'weather-data.json');
+const saveKeyValue = async (key, value) => {
+  let data = {};
+  if (await isExist(filePath)) {
+    const file = await promises.readFile(filePath);
+    data = JSON.parse(file);
+  }
+  data[key] = value;
+  await promises.writeFile(filePath, JSON.stringify(data));
 };
 
+const getKeyValue = async (key) => {
+  if (await isExist(filePath)) {
+    const file = await promises.readFile(filePath);
+    const data = JSON.parse(file);
+    return data[key];
+  }
+  return undefined;
+};
 
-export { saveKeyValue };
+const isExist = async (path) => {
+  try {
+    await promises.stat(path);
+    return true;
+  } catch (e) {
+    console.log('isExist: false');
+    return false;
+  }
+};
+
+export { saveKeyValue, getKeyValue };
--- weather-cli/weather.js
+++ weather-cli/weather.js
@@ -3,6 +3,14 @@ import { getArgs } from './helpers/args.js';
 import { printError, printSuccess, printHelp } from './services/log-service.js';
 import { saveKeyValue } from './services/storage-service.js';
 
+const saveToken = async (token) => {
+	try {
+		await saveKeyValue('token', token);
+		printSuccess('Token save');
+	} catch (e) {
+		printError(e.message);
+	}
+};
 
 const initCLI = () => {
 	const args = getArgs(process.argv);
@@ -13,7 +21,7 @@ const initCLI = () => {
 		// сохранить город
 	}
 	if (args.t) {
-		saveKeyValue('token', args.t);
+		return saveToken(args.t);
 	}
 	// вывести погоду
 };
