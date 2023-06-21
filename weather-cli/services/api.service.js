dd8d8406df94fd00c91aac4cc2b058a696162a63
Dima Mihnevich <dima.mihnevich@gmail.com>
2023-06-21 11:12:39 +0300

    Взаимодействие с API
    axios

--- weather-cli/services/api.service.js
+++ weather-cli/services/api.service.js
@@ -1,26 +1,22 @@
 import https from 'https';
 import { getKeyValue, TOKEN_DICTIONARY } from './storage-service.js';
+import axios from 'axios';
 
 const getWeather = async (city) => {
   const token = await getKeyValue(TOKEN_DICTIONARY.token);
   if (!token) {
     throw new Error('API key doesn\'t set, use api key through command -t [API_KEY]');
   }
-  const url = new URL('https://api.openweathermap.org/data/2.5/weather');
-  url.searchParams.append('q', city);
-  url.searchParams.append('appid', token);
-  url.searchParams.append('lang', 'ru');
-  url.searchParams.append('units', 'metric');
 
-  https.get(url, (response) => {
-    let result = '';
-    response.on('data', (chunk) => {
-      result += chunk;
-    });
-    response.on('end', () => {
-      console.log(result);
-    });
+  const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+    params: {
+      q: city,
+      appid: token,
+      lang: 'ru',
+      units: 'metric'
+    }
   });
+  return data;
 };
 
 export { getWeather };
