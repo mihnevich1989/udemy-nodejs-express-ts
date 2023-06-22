6d763ea34288571a706b8de148f523ad10e3265d
Dima Mihnevich <dima.mihnevich@gmail.com>
2023-06-22 21:53:37 +0300

    Маршрутизация

--- express-server/index.js
+++ express-server/index.js
@@ -3,9 +3,22 @@ const app = express();
 const host = '127.0.0.1';
 const port = 8000;
 
-app.get('/hello', (req, res) => {
-	res.send('Hello World');
+app.all('/hello', (req, res, next) => {
+	console.log('all');
+	next();
 });
+
+const cb = (req, res, next) => {
+	console.log('CB');
+	next();
+};
+app.route('/user')
+	.get('/hello', [cb, cb, cb, (req, res) => {
+		res.send('GET: Hello');
+	}])
+	.post('/hello', (req, res) => {
+		res.send('POST: Hello');
+	});
 
 app.listen(port, () => {
 	console.log('Server online', host, ':', port);
