bba612cf57257c28384b00976b647f52e3ba3f49
Dima Mihnevich <dima.mihnevich@gmail.com>
2023-06-22 21:34:05 +0300

    express js

--- express-server/index.js
+++ express-server/index.js
@@ -1,22 +1,12 @@
-import http from 'http';
-
+import express from 'express';
+const app = express();
 const host = '127.0.0.1';
 const port = 8000;
 
-const server = http.createServer((req, res) => {
-	switch (req.method) {
-		case 'GET':
-			switch (req.url) {
-				case '/hello':
-					res.statusCode = 200;
-					res.setHeader('Content-Type', 'text/plane');
-					res.end('Hellow world');
-					break;
-			}
-			break;
-	}
+app.get('/hello', (req, res) => {
+	res.send('Hello World');
 });
 
-server.listen(port, host, () => {
+app.listen(port, () => {
 	console.log('Server online', host, ':', port);
 });
