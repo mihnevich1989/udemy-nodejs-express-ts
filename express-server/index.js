151da6ab744fee485afa90a0b27d019883484981
Dima Mihnevich <dima.mihnevich@gmail.com>
2023-06-22 22:11:44 +0300

    Ответы клиенту

--- express-server/index.js
+++ express-server/index.js
@@ -3,22 +3,18 @@ const app = express();
 const host = '127.0.0.1';
 const port = 8000;
 
-app.all('/hello', (req, res, next) => {
-	console.log('all');
-	next();
-});
-
-const cb = (req, res, next) => {
-	console.log('CB');
-	next();
-};
-app.route('/user')
-	.get('/hello', [cb, cb, cb, (req, res) => {
-		res.send('GET: Hello');
-	}])
-	.post('/hello', (req, res) => {
-		res.send('POST: Hello');
+app.get('/hello', (req, res) => {
+	res.cookie('token', 'sdawqdwqcasdqdcaxsc1222331dsasd', {
+		domain: '',
+		path: '/',
+		secure: true,
+		expires: new Date()
 	});
+	res.clearCookie('token');
+	console.log('GET method');
+	// res.send('Hi');
+	res.end();
+});
 
 app.listen(port, () => {
 	console.log('Server online', host, ':', port);
