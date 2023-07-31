0e70238924a35125c930ef99356c8e6a86a0ea4e
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2023-07-31 10:37:50 +0300

    Middleware для роутов

--- express-server/src/common/base.controller.ts
+++ express-server/src/common/base.controller.ts
@@ -32,8 +32,10 @@ export abstract class BaseController {
   protected bindRoutes(routes: IControllerRoute[]): void {
     for (const route of routes) {
       this.logger.log(`[${route.method}] ${route.path}`);
+      const middleware = route.middlewares?.map((m) => m.execute.bind(m));
       const handler = route.func.bind(this); // для связки контекста текущего класса с нашей функцией, потому как после вызова route.func он бы потерялся и был бы как контекст функции func
-      this.router[route.method](route.path, handler);
+      const pipeline = middleware ? [...middleware, handler] : handler;
+      this.router[route.method](route.path, pipeline);
     }
   }
 }
