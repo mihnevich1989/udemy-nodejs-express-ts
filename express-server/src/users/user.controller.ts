5a2bf0cf8c2bb3a14913b6b9428185fef238685b
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2023-08-03 10:48:41 +0300

    Репозиторий users

--- express-server/src/users/user.controller.ts
+++ express-server/src/users/user.controller.ts
@@ -8,7 +8,6 @@ import 'reflect-metadata';
 import { IUserController } from './user.controller.interface';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
-import { User } from './user.entity';
 import { IUserService } from './users.service.interface';
 import { ValidateMiddleware } from '../common/validate.middleware';
 
@@ -44,6 +43,6 @@ export class UserController extends BaseController implements IUserController {
     if (!result) {
       return next(new HTTPError(422, 'Такой пользователь уже существует'));
     }
-    this.ok(res, result);
+    this.ok(res, { email: result.email, id: result.id });
   }
 }
