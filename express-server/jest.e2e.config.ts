d5ef50bde1ba6edd091ffacb67258b3fcdb038f8
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2023-08-10 13:35:05 +0300

    Дописываем e2e тесты

--- express-server/jest.e2e.config.ts
+++ express-server/jest.e2e.config.ts
@@ -3,7 +3,6 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   verbose: true,
   preset: 'ts-jest',
-  rootDir: './src/tests',
   testRegex: '.e2e-spec.ts$',
 };
 
